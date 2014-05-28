70a0c3681cc42a3d916cbdee901a97c5109c7bd7
Sparking <Sparking@users.noreply.github.com>
2014-05-28 09:56:28 +0000

    Create version.h
    Other infos.

---
+++ version.h
@@ -0,0 +1,18 @@
+/* Project: Date Counter
+ * Name: version.h
+ * Author: Sparking
+ * Built time: 2014-4-11 12:48
+ */
+
+#ifndef COUNTER_VERSION_H_
+#define COUNTER_VERSION_H_
+
+#define VER "0"
+#define VERSION_MAJ "0"
+#define VERSION_MIN "2"
+#define VERSION VER"."VERSION_MAJ"."VERSION_MIN
+#define PRGNAME "Date Counter"
+#define AUTHORS "Sparking"
+#define HELP "Available options:\n  -h,--help        show help\n  -v,--version     show version information\n  -a,--add         work out the date by days\n  -f,--from  date  with the first date following (format like 2014 3 1)\n  -t,--to  date    whith the second date following (format like 2014 3 1)\n"PRGNAME
+
+#endif

d4b51e6895f83e0d26fee80ae19703e236e7c033
Sparking <Sparking@users.noreply.github.com>
2014-06-16 22:39:33 +0800

    Update main.c

--- main.c
+++ main.c
@@ -33,6 +33,10 @@
 #define IS(X) (strcmp(X,argv[i]) == 0)//idea is from the file luac.c of lua5.2.3
 
 static int getfirst(void)
+/* This function will get the 
+ * first character of one-line
+ * input and will return it.
+ */
 {
     int ch;
     ch = getchar();
